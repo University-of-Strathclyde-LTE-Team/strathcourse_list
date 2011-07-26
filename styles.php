0fe815df57d9c61e84803d3edf8642ef16277fdf
mhughes2k <michaelhughes@strath.ac.uk>
2011-07-26 15:36:23 +0000

    Disabled icon for the moment
    git-svn-id: https://svn.strath.ac.uk/repos/moodle/plugins/block-strathcourse_list/trunk@1885 19f72610-e705-46c6-848a-4f7065363fcc

--- styles.php
+++ styles.php
@@ -1,11 +1,11 @@
 .strathcourse {
 }
 .block_strathcourse_list > .header{
-//    height:21px;
+//    height:21px;/*
     background-size:30px;
     background-image:url('<?php echo $CFG->wwwroot;?>/blocks/strathcourse_list/course.gif');
-    background-repeat:no-repeat;
+    background-repeat:no-repeat;*/
 }
 .block_strathcourse_list > .header > .title{
-    margin-left:32px;
+    /*margin-left:32px;*/
 }
