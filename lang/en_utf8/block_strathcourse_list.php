e774765174dc1010bd8ced3a2c2b4e670bdefdc2
mhughes2k <michaelhughes@strath.ac.uk>
2011-07-28 15:07:19 +0000

    Language file
    git-svn-id: https://svn.strath.ac.uk/repos/moodle/plugins/block-strathcourse_list/trunk@1900 19f72610-e705-46c6-848a-4f7065363fcc

---
+++ lang/en_utf8/block_strathcourse_list.php
@@ -0,0 +1,8 @@
+<?Php
+
+//$string['block_course_list_archiveserversnames'] = 'Archive S
+$string['archiveservers_help']       =   'The <em>base</em> hostname for archived servers. Archive servers are queried in order listed.<br /> 1 Per Line.';
+$string['archiveservernames_help']  =   'There should be a name for each server listed in <code>block_course_list_archiveservers</code>. 1 Per Line';
+$string['degreeprogrammesite']      =   'Course';
+$string['youreanlta']   =   'You are an LTA';
+$string['ltacourses']   =   'LTA Course Access';
